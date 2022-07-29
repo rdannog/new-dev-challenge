007883cc85898eaaea9ee9ab66489a8b525fd6c3
rdannog <dandaro.nogueira@gmail.com>
2022-07-29 01:13:32 -0300

    feature: rendering random species on click

--- src/pages/species.js
+++ src/pages/species.js
@@ -6,30 +6,44 @@ import * as S from "../components/styles"
 
 export default class Species extends React.Component {
   state = {
-    speciesList: []
+    speciesList: [],
+    randomSpecie:"Get a random Star Wars specie!"
   };
 
   async componentDidMount() {
     const response = await Api.data.get("/species");
     this.setState({
       speciesList: response.data.results
-    });
-    console.log(response)
+    })
   }
+  randomIndex = (a, z) => {
+    return Math.floor(Math.random() * (z - a)) + a;
+  };
+    
+  handleClick = () => {
+    const {speciesList} = this.state
+    const allSpecies = speciesList.map((specie,i)=>(
+      <S.Card key={i}>
+      <h2>{specie.name}</h2>
+      <p>Language: {specie.language}</p>
+      <p>Skin colors: {specie.skin_colors}</p>
+    </S.Card>
+    ))
+    this.setState({
+      randomSpecie: allSpecies[this.randomIndex(0, allSpecies.length)]
+    });
+  };
 
   render() {
-    const {speciesList} = this.state
+    const {randomSpecie} = this.state
     return (
       <>
         <Header/>
         <S.Container>
-        {speciesList.map((specie,i)=>(
-          <S.Card key={i}>
-          <h2>{specie.name}</h2>
-          <p>Language: {specie.language}</p>
-          <p>Skin colors: {specie.skin_colors}</p>
-        </S.Card>
-        ))}
+          <S.Card>
+            <h2>{randomSpecie}</h2>
+          </S.Card>
+          <S.Button onClick={this.handleClick}>Randomize</S.Button>
         </S.Container>
       </>
     );
